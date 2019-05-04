4616e95de582596ad310c1f0f4ede798af815a70
SP SL <sp.selflearning@outlook.com>
2019-05-04 16:52:27 +0200

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+console.log("Client side javascript file is loaded!");
+
+// fetch("http://puzzle.mead.io/puzzle").then(response => {
+//   response.json().then(data => {
+//     console.log(data);
+//   });
+// });
+
+const weatherForm = document.querySelector("form");
+const searchElement = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const location = searchElement.value;
+  messageOne.textContent = "Loading...";
+  //   console.log(location);
+  fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        // console.log(data.error);
+        messageOne.textContent = `${data.error}`;
+      } else {
+        // console.log(data.location);
+        // console.log(data.forecast);
+        messageOne.textContent = `${data.location} , ${data.forecast}`;
+      }
+    });
+  });
+});
